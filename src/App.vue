ce8d670514f8213aa99a31a5ab4bc9541b07c51d
marpri <mark.pristup@kronshtadt.ru>
2021-03-24 19:52:22 +0300

    negin to change design

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,102 @@
 <template>
   <div id="app">
 
-    <div id="nav">
-      <router-link to="/">Form</router-link> |
-      <router-link to="/admin">Admin</router-link> |
-      <router-link to="/sales">Sales</router-link>
+    <!--    <div id="nav">-->
+    <!--      <router-link to="/">Form</router-link> |-->
+    <!--      <router-link to="/admin">Admin</router-link> |-->
+    <!--      <router-link to="/sales">Sales</router-link>-->
+    <!--    </div>-->
+
+    <!--    Продажа нерудных материалов и аренда спецтехники в СПб и области -->
+    <!--    Работаем 24/7-->
+    <!--    Заказать звонок-->
+
+    <div class="head">
+      <div class="container">
+        <span>Продажа нерудных материалов и аренда спецтехники в СПб и области</span>
+      </div>
     </div>
 
-    <router-view/>
+    <div class="logo">
+      <a href="https://idealnerud.ru"><img src="@/assets/logo.png" alt=""></a>
+    </div>
+
+    <div class="content">
+      <div class="centered">
+        <div class="in">
+
+          <router-view/>
+
+        </div>
+      </div>
+    </div>
+
+    <footer class="footer">
+      <div class="container">
+        <span>© 2021</span><span class="px-2">«ООО ИДЕАЛТРЕЙД»</span><a href="https://idealnerud.ru/policy/">Политика
+        конфиденциальности</a>
+      </div>
+    </footer>
 
   </div>
 </template>
 
 <style>
+html, body {
+  height: 100%;
+}
+
 #app {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
   text-align: center;
 }
 
-#nav {
+.content {
+  flex: 1 0 auto;
+  padding: 20px;
+}
+
+.centered {
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.centered .in {
+  margin: 0;
+}
+
+.footer {
+  flex-shrink: 0;
+  padding: 20px;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.logo {
+  background: #ffbc13;
   padding: 30px;
+  margin-bottom: 30px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.head {
+  background: #1b1b1b;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+  font-size: 14px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.footer {
+  color: #fff;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 20px;
+  background: #1b1b1b;
 }
 </style>
