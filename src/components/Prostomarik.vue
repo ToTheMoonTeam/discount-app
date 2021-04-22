9b0f5f0fabfdd03cb24fdad360468541f5ff396f
prostomarik <ma2011rk@gmail.com>
2021-04-23 00:14:01 +0300

    made design

--- src/components/Prostomarik.vue
+++ src/components/Prostomarik.vue
@@ -2,7 +2,9 @@
   <div class="prostomarik">
     <div class="btn-wrapper">
       <div class="btn">
+        <img src="@/assets/moon.jpg" alt="">
         <h1>Разрабатываем неразрабатываемое</h1>
+        <a href="https://github.com/ToTheMoonTeam">github.com/ToTheMoonTeam</a>
       </div>
     </div>
   </div>
@@ -16,7 +18,7 @@ export default {
 
 <style scoped>
 .prostomarik {
-  background: #E8F1FA;
+  background: #000000;
   height: 100%;
   width: 100%;
 }
@@ -26,7 +28,8 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #E8F1FA;
+  background: #000000;
+  padding-bottom: 170px;
 }
 
 .prostomarik .btn {
@@ -34,6 +37,22 @@ export default {
   /*border-radius: 25px;*/
   /*background: #c5cdd5;*/
   /*padding: 20px 40px;*/
+}
+
+.btn img {
+  width: 400px;
+  height: 400px;
+}
+
+.btn h1 {
+  margin-top: 30px;
+  margin-bottom: 7px;
+  color: #f0f2f3;
+}
+
+.btn a {
+  font-size: 20px;
+  text-decoration: none;
 }
 
 </style>
